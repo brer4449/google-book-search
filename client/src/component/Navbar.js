1af92a3181fa6c6622a2aad58b9b774c675f4b02
brer4449 <58540640+brer4449@users.noreply.github.com>
2020-03-10 10:34:19 -0400

    added more initial components

---
+++ client/src/component/Navbar.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-light">
+      <a className="navbar-brand"><span className="logo">Bid</span>Bot</a>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="nav navbar-nav ml-auto">
+
+          <li className="nav-item">
+            <Link
+              to="/"
+              className={
+                window.location.pathname === "/"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Home <span> |</span>
+            </Link>
+          </li>
+          <li className="nav-item categories">
+            <Link
+              to="/bid"
+              className={window.location.pathname === "/bid" ? "nav-link active" : "nav-link"}
+            >
+              Categories <span> |</span>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/post"
+              className={window.location.pathname === "/post" ? "nav-link active" : "nav-link"}
+            >
+              Add item
+            </Link>
+          </li>
+
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
