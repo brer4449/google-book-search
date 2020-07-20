8a05a3539b479f000e56fb9a64877ebd4d682031
brer4449 <58540640+brer4449@users.noreply.github.com>
2020-07-20 13:27:46 -0700

    added atlas MongoURI and functionality, no more errors

--- Master/server.js
+++ Master/server.js
@@ -1,9 +1,16 @@
 const express = require("express");
-
-const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+// const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
+const connectDB = require("./config/db");
 const PORT = process.env.PORT || 3001;
+
+// Load env vars (config file)
+dotenv.config({ path: "./config/config.env" });
+
+// Connect to the Mongo DB
+connectDB();
 
 // Configure body parsing for AJAX requests
 app.use(express.urlencoded({ extended: true }));
@@ -16,16 +23,27 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
-// Connect to the Mongo DB
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://user1:password1@ds125871.mlab.com:25871/heroku_0xn0jnk7",
-  {
-    useCreateIndex: true,
-    useNewUrlParser: true
-  }
-);
+// mongoose.connect(
+//   process.env.MONGODB_URI ||
+//     "mongodb://user1:password1@ds125871.mlab.com:25871/heroku_0xn0jnk7",
+//   {
+//     useCreateIndex: true,
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   }
+// );
 
 // Start the API server
-app.listen(PORT, () =>
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+const server = app.listen(PORT, () =>
+  console.log(
+    `🌎  ==> API Server running in ${process.env.NODE_ENV} mode on PORT ${PORT}!`
+  )
 );
+
+// Global handler for all unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server and exit process
+  // want it to exit with failure so pass in 1 in order to do that
+  server.close(() => process.exit(1));
+});
